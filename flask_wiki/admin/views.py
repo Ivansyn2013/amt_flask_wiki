a710a59859d726a1206b6fc512c3537b7c099503
ivan <ivansyn2013@yandex.ru>
2023-09-08 19:42:46 +0300

    copy admin static and correct nginx

--- flask_wiki/admin/views.py
+++ flask_wiki/admin/views.py
@@ -2,6 +2,7 @@ from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from flask import redirect, url_for, request
 
+
 class MyAdminView(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated and current_user.is_admin
---
+++ flask_wiki/collect_admin_static.py
@@ -0,0 +1,12 @@
+from flask_admin import static
+import os
+import shutil
+import flask_admin
+
+if __name__ == "__main__":
+
+    path = os.path.dirname(flask_admin.__file__)
+    src = path + '/static'
+    destination = os.getcwd() + '/static' + '/admin'
+    shutil.copytree(src, destination)
+    #print(path)
