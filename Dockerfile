8f8dad6eb2dcfe2300d1fe8e984c9a01c0050b1a
ivan <ivansyn2013@yandex.ru>
2023-09-08 16:01:17 +0300

    edit config for deploy

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
-FROM python:3.10-buster
+FROM ivanvic/amt_wiki_req:v1
 WORKDIR /app
 COPY . /app/
-ENV ENV_PATH="/app/.env"
-RUN pip install poetry
-RUN poetry config virtualenvs.create false && poetry install \
-    --no-interaction --no-ansi
-RUN pip install gunicorn
+#RUN pip install poetry
+#RUN poetry config virtualenvs.create false && poetry install \
+#    --no-interaction --no-ansi
+#RUN pip install gunicorn
 RUN flask db migrate
 RUN flask db upgrade
 CMD ["gunicorn", "wsgi:app", "-b", "0.0.0.0:5006"]
--- examples/app.py
+++ examples/app.py
@@ -40,13 +40,13 @@ def create_app(test_config=None):
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
-        app.config.from_object(Develop)
+        app.config.from_object(Deploy)
     else:
         # load the test config if passed in
         app.config.from_pyfile('config.py', silent=True)
         # name of var with env path
         app.config.from_object(Deploy)
-        app.config.from_envvar(test_config)
+        #app.config.from_envvar(test_config)
         print(app.config)
 
     Bootstrap4(app)
