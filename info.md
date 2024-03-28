34d966ebf9a6cbbd07783934fa8f6e9a6abdd84a
syn <ivansyn2013@yandex.ru>
2024-03-28 12:03:06 +0000

    change db script

--- info.md
+++
@@ -1,5 +0,0 @@
-Получить даты веток \
-``git for-each-ref --format '%(refname:short) %(objectname:short) %(committerdate:short) %(contents:subject) [%(upstream:short)]' refs/heads/*
-``
-
-Применить миграции
--- scripts/db_entripoints/db_enry_point.sh
+++ scripts/db_entripoints/db_enry_point.sh
@@ -7,7 +7,7 @@ CREATE_ROLE="CREATE ROLE $PGUSER WITH LOGIN PASSWORD '$PGPASSWORD';"
 CREATE_DB="CREATE DATABASE IF NOT EXISTS $PGDB;"
 CREATE_ROLE="CREATE ROLE IF NOT EXISTS $PGUSER WITH LOGIN PASSWORD '$PGPASSWORD';"
 GRANT_PREV="GRANT ALL PRIVILEGES ON DATABASE $PGDB TO $PGUSER;"
-
+CHANGE_TABLES_OWNER="SELECT 'ALTER TABLE public.' || tablename || ' OWNER TO $PGUSER;' FROM pg_tables WHERE schemaname = 'public';"
 
 wait_for_postgres() {
 
@@ -20,14 +20,14 @@ wait_for_postgres() {
 
 wait_for_postgres 
 
-psql -U postgres -c "$CREATE_DB" 
-psql -U postgres -c "$CREATE_ROLE" 
-psql -U postgres -c "$GRANT_PREV" 
-
+psql -U postgres -c "$CREATE_DB" &&           echo "Create db $PGDB success"
+psql -U postgres -c "$CREATE_ROLE" &&         echo "Create role $PGUSER success" 
+psql -U postgres -c "$GRANT_PREV" &&          echo "Create grant prev success"     
+psql -U postgres -c "$CHANGE_TABLES_OWNER" && echo "Create table owner change success" 
 
 if [ -f "$PATH_TO_SQL" ]; then
     # Restore the backup file
-    $POSTRGES_PASSWORD psql -U postgres -d "$PGDB" -f $PATH_TO_SQL
+    $POSTRGES_PASSWORD psql -U postgres -d "$PGDB" < $PATH_TO_SQL
 
     echo "Dump upladed"
 	else
