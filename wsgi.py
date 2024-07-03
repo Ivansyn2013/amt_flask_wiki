b9e1e870aa4fb00050518cd45f26b7b11dd3efa4
ivan_note <ivansyn2013@yandex.ru>
2024-07-03 12:00:07 +0300

    - edir result rendering

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,12 @@
+from examples.app import app
 from examples.app import create_app
 import logging
 from dotenv import load_dotenv
 from flask_wiki.context_processor import inject_quiz_nums_context
-
+#возможно gunicorn не выполняет код ниже, я просто берет app из верхней строчки
 
 if __name__ == '__main__':
     load_dotenv()
     logging.basicConfig(level=logging.DEBUG)
     app = create_app()
-    app.context_processor(inject_quiz_nums_context)
-    app.run(debug=True)
+    app.run()
